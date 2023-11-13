1fe82d3ff42e006ea030f39dd09ad4ceacaa3e23
mrtewogbade <johndharey@gmail.com>
2023-11-13 22:48:30 +0100

    updated\

--- main.js
+++ main.js
@@ -1,31 +1,49 @@
-
-const btn = document.querySelector ('.add-book');
+const addBookBtn = document.querySelector('.add-book');
 const submitBtn = document.querySelector ('.submit');
 const form = document.querySelector('form');
 
 
-btn.addEventListener('click', () =>{
+
+
+addBookBtn.addEventListener('click', () => {
      form.style.display = 'flex';
 })
 
-console.log(btn);
 
 
+const errorMsg = querySelector('#para');
+const title = document.querySelector('#title')
+const author = document.querySelector('#author')
+const pages = document.querySelector('#pages')
+const isRead = document.querySelector('#isRead')
 
-// function validateForm(){
-//     const paraEl = document.getElementById('para');
-//     const title = document.querySelector('#title').value;
-//     const author = document.querySelector('#author').value;
-//     const pages = document.querySelector('#pages').value;
-//     const isRead = document.querySelector('#isRead').value;
+const inputs = [author, pages, isRead, title]
 
-//     if (title === '' || author === '' || pages === '' || (isRead !== 'yes' && isRead !== 'no')){
-//         paraEl.textContent = 'Please fill in all fields'
-//         return false;
-//     }
-//     return true;
-// }
-// submitBtn.addEventListener('click', validateForm)
+inputs.forEach((input) => {
+    input.addEventListener('focusin', () =>{
+        errorMsg.textContent = '';
+        item.classList.remove('error');
+
+    })
+});
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const isRead = document.querySelector('#isRead').value;
+
+    if(title === '' || author === '' || pages === '' || isRead === ''){
+        errorMsg.textContent = 'Please fill in all fields';
+        item.classList.add('error');
+    } else {
+        addBookToLibrary(title, author, pages, isRead);
+        form.style.display = 'none';
+    }
+
+})
 
 const myLibrary = [];
 
